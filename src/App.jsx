691573fd94e69e8a921d822170bd37c23733c342
Jeovani Agostini <joba.jalsh@gmail.com>
2022-02-02 14:41:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import WidgetTree from './components/CheckboxTree';
+
+function App() {
+  return (
+    <div className="App">
+      <WidgetTree />
+    </div>
+  )
+}
+
+export default App
